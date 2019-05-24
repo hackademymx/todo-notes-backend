dd5927d85986efc66e429c0b47fce9801bfaf54d
Rodrigo M <rodrigo.medina.neri@gmail.com>
2019-05-23 19:27:58 -0500

    Update index.js file

--- index.js
+++ index.js
@@ -1,72 +1,18 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
-const { TodoNote } = require('./db/schemas');
+const todoNoteOps = require('./entities/TodoNote/operations');
 
 // Se usa para poder revisar el contenido del body dentro de las peticiones http
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('TODO api de Rodrigo'));
 
-// Create
-app.post('/todo', (req, res) => {
-  // Crear nota
-  const {
-    title,
-    body,
-    date
-  } = req.body;
+app.route('/todos')
+  .get(todoNoteOps.getAllTodoNotes)
+  .post(todoNoteOps.createNewTodoNote)
+  .patch(todoNoteOps.updateSpecificTodoNote)
+  .delete(todoNoteOps.deleteSpecificNote);
 
-  const newTodo = new TodoNote({ title, body, date });
-  newTodo.save((err, savedObject) => {
-    return err
-      ? res.send('Ocurrió un error')
-      : res.send('Nota guardada con éxito');
-  });
-
-});
-
-// Read
-app.get('/todos', (req, res) => {
-  TodoNote.find((err, todosList) => {
-    return err
-      ? res.send('Ocurrió un error')
-      : res.send(todosList);
-  });
-});
-
-app.get('/todos/:documentId', (req, res) => {
-  const { documentId } = req.params;
-
-  TodoNote.find({ _id: documentId }, (err, result) => {
-    return err
-      ? res.send('Ocurrió un error')
-      : res.send(result[0]);
-  });
-});
-
-// Update
-app.patch('/todo', (req, res) => {
-  const { documentId, title } = req.body;
-
-  TodoNote.find({ _id: documentId }, (err, result) => {
-    const editedNote = new TodoNote(result[0]);
-    editedNote.title = title;
-    editedNote.save((err, savedObject) => {
-      return err
-        ? res.send('Ocurrió un error')
-        : res.send('Nota editada con éxito');
-    })
-  });
-});
-
-// Delete
-app.delete('/todo', (req, res) => {
-  const { documentId, title } = req.body;
-  TodoNote.findOneAndDelete({ _id: documentId }, (err, result) => {
-    return err
-      ? res.send('Ocurrió un error')
-      : res.send('Nota borrada con éxito');
-  });
-});
+app.get('/todos/:documentId', todoNoteOps.getSpecificTodoNote);
 
 app.listen(5000, () => console.log('Servidor levantado'));
